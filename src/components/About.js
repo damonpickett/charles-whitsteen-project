6713ee49c4420c03028d9522bbd2dd34f6d4c43b
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-07-29 13:45:29 -0700

    added background color and font colors

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ function About() {
           <h2>The Story</h2>
           <p>Join Charles Whitsteen as he probes the depths of reality and turns it inside out to remind us that not all is not what it seems. And along the way we'll  <a href='https://flyingfurrescue.wixsite.com/rescue' target='_blank' rel='noreferrer'>save cute little animals</a>.</p>
         </div>
-        <div classnName='community'>
+        <div className='community'>
           <h2>The Community</h2>
           <p>The Charles Whitsteen Project community is a place where all are welcome, as long as you're a weirdo. Vulgar heresies may be spoken.</p>
           <button>Join Discord</button>
