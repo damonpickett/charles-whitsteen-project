787e3b1703ffeb865f1119b9703a23a20a5bb09f
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-07-29 11:42:15 -0700

    added font, resized and saved images, modified components, created social component

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -3,7 +3,7 @@ import '../App.css';
 function Intro() {
   return (
       <div className='intro'>
-        <div className='disclaimer'>* This is a mock NFT project. It is for display purposes only. The NFTs below can only be purchased with mock matic and can only be displayed on OpenSea's testnet site.</div>
+        <div className='disclaimer'>* This is a mock NFT project. It is for demonstration purposes only. The NFTs below can only be purchased with mock matic and can only be displayed on OpenSea's testnet site.</div>
         <h1>The Charles Whitsteen Project</h1>
         <p>The Charles Whitsteen Project gives collectors the chance to own original artwork by Charles Whitsteen.</p>
         <p>Every purchase helps fund <a href='https://flyingfurrescue.wixsite.com/rescue' rel='noreferrer' target='_blank'>Flying Fur Rescue</a> to help save doggies, kitties, and other friendly little critters.</p>
---
+++ src/components/Social.js
@@ -0,0 +1,12 @@
+import '../App.css';
+
+function Social() {
+  return (
+      <div className='social'>
+        <div className='social-block'>Charles Whitsteen</div>
+        <div className='social-block'>Damon Pickett</div>
+      </div>
+  );
+}
+
+export default Social;
