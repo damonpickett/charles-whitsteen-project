b8709c83b95812a0e00abdfe683858354759ddd4
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-07-30 12:00:44 -0700

    added increment and mint buttons to piece component, style only

--- src/components/Piece.js
+++ src/components/Piece.js
@@ -1,10 +1,42 @@
 import '../App.css';
 
 function Piece(props) {
+
+  const handleDecrement = () => {
+    if (props.mintAmount <= 1) return;
+    props.setMintAmount(props.mintAmount - 1);
+  };
+
+  const handleIncrement = () => {
+    if (props.mintAmount >= 3) return;
+    props.setMintAmount(props.mintAmount + 1);
+  };
+
   return (
       <div className='piece'>
-        <h3>{props.title}</h3>
-        <img src={props.src} alt={props.alt}></img>
+        <div className='image'>
+          <h3>{props.title}</h3>
+          <img src={props.src} alt={props.alt}></img>
+        </div>
+        <div className='increments'>
+          <button
+            className='incdec'
+            onClick={handleDecrement}
+          >-</button>
+          <input
+            type='number'
+            value={props.mintAmount}
+          ></input>
+          <button
+            className='incdec'
+            onClick={handleIncrement}
+          >+</button>
+        </div>
+        <div className='mint-button'>
+            <button 
+              onClick={props.onClick}
+            >Mint</button>
+          </div>
       </div>
   );
 }
