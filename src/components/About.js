56d81da7bce21e266c81076278f1d73f4a81f7df
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-07-30 20:07:49 -0700

    added text content to about component

--- src/components/About.js
+++ src/components/About.js
@@ -9,17 +9,22 @@ function About() {
           <h2>The Story</h2>
           <div className='content'>
             <img src={metaMan} alt='Meta Man'></img>
-            <p>Join Charles Whitsteen as he probes the depths of reality and turns it inside out to remind us that not all is not what it seems. And along the way we'll  <a href='https://flyingfurrescue.wixsite.com/rescue' target='_blank' rel='noreferrer'>save cute little animals</a>.</p>
+            <div className='content-text'>
+              <p>We live in a world of masks. Glossy smooth surfaces hide the hideous truth within. To many, this is a blessing, a tender mercy. But there are some who wish to go deeper. There are some determined to walk the path of truth no matter the cost. Charles Whitsteen, with sight and skill, is leading us down that path. Join Whitsteen as he probes the depths of reality, turns them inside out and reminds us not all is what it seems. And we'll make some <a href='https://flyingfurrescue.wixsite.com/rescue' rel='noreferrer' target='_blank'>furry friends</a> along the way.</p>
+              <button>Instagram</button>
+            </div>
           </div>
-          <button>Instagram</button>
         </div>
         <div className='community'>
           <h2>The Community</h2>
           <div className='content'>
-            <p>The Charles Whitsteen Project community is a place where all are welcome, as long as you're a weirdo. Vulgar heresies may be spoken.</p>
+            <div className='content-text'>
+              <p>Remove your mask and be the delightful ghoul that you are. The Charles Whitsteen Project community is a place where all are welcome, as long as you're a weirdo. Vulgar heresies may be spoken. Smiles are encouraged.</p>
+              <button>Join Discord</button>
+            </div>
             <img src={insaneParade} alt='The Green Room'></img>
+            
           </div>
-          <button>Join Discord</button>
         </div>
       </div>
   );
