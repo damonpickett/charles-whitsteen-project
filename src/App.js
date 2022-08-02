02508830c91e544e6a96a5304ea6be6ac8d2f87c
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-08-02 16:21:07 -0700

    installed react-icons, began building burger menu

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Nav from './components/Nav';
 import Home from './components/Home';
@@ -6,10 +7,14 @@ import Roadmap from './components/Roadmap';
 import Social from './components/Social';
 import Footer from './components/Footer';
 
+
 function App() {
+
+  const [menu, setMenu] = useState(false);
+
   return (
     <div className="App">
-      <Nav />
+      <Nav menu={menu}/>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/roadmap' element={<Roadmap />} />
