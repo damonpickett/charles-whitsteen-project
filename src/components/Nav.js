7a9ec9108f85eebae334705d5e862863d5df916c
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-08-05 10:22:04 -0700

    adjusted max-width on nav and about for larger screens

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,30 +4,29 @@ import * as FaIcons from 'react-icons/fa';
 
 function Nav(props) {
   
-  
-
   return (
     <>
       <div id='home'></div>
         <div className='nav'>
-          
-          <ul className='full-screen-menu'>
-            <li><Link smooth to='/#home'>Home</Link></li>
-            <li><Link smooth to='/#collection'>Collection</Link></li>
-            <li><Link smooth to='/#about'>About</Link></li>
-            <li><Link to='/roadmap'>Roadmap</Link></li>
-            <li><Link to='/social'>Social</Link></li>
-          </ul>
-          
-          {/* for mobile */}
-          <div className='burger'>
-            <FaIcons.FaBars onClick={() => props.setMenu(true)}/>
-          </div>          
-          
-          <ul className='nav-buttons'>
-            <li><button>Instructions</button></li>
-            <li><button>Connect Wallet</button></li>
-          </ul>
+          <div className='nav-container'>
+            <ul className='full-screen-menu'>
+              <li><Link smooth to='/#home'>Home</Link></li>
+              <li><Link smooth to='/#collection'>Collection</Link></li>
+              <li><Link smooth to='/#about'>About</Link></li>
+              <li><Link to='/roadmap'>Roadmap</Link></li>
+              <li><Link to='/social'>Social</Link></li>
+            </ul>
+            
+            {/* for mobile */}
+            <div className='burger'>
+              <FaIcons.FaBars onClick={() => props.setMenu(true)}/>
+            </div>          
+            
+            <ul className='nav-buttons'>
+              <li><button>Instructions</button></li>
+              <li><button>Connect Wallet</button></li>
+            </ul>
+          </div>
         </div>
     </>
   );
