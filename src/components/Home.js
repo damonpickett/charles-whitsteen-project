830707ff54fef83fef8972f3ae6862b5bad88f3f
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-08-17 20:38:18 -0700

    fixed type in disclaimer

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ function Intro(props) {
   return (
     <>
       <div className='intro'>
-        <div className='disclaimer'>* This is a mock NFT project. It is for demonstration purposes only. The NFTs below can only be purchased with mock matic and can only be displayed on OpenSea's testnet site.</div>
+        <div className='disclaimer'>* This is a mock NFT project. It is for demonstration purposes only. The NFTs below can only be purchased with mock Ethereum and can only be displayed on OpenSea's testnet site.</div>
         <h1>The Charles Whitsteen Project</h1>
         <p>The Charles Whitsteen Project gives collectors the chance to own original artwork by Charles Whitsteen.</p>
         <p>Every purchase helps fund <a href='https://flyingfurrescue.wixsite.com/rescue' rel='noreferrer' target='_blank'>Flying Fur Rescue</a> to help save doggies, kitties, and other friendly little critters.</p>
